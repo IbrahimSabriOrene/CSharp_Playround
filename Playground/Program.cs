91c248ff5ab3a401e6b51ce355cecb6a423b69c2
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-08-20 15:58:14 +0300

    start of the playground

---
+++ Playground/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Playground;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // I will be using this file to test out code
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("");
+    }
+}
